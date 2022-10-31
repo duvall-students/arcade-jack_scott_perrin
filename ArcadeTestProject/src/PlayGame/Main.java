2f4d409db37e179e6aebc9a43aa5e7f788bfde9e
ssumrall <78440487+scottsumrall@users.noreply.github.com>
2022-10-31 16:34:03 -0400

    Fully functional except for entity deletion

---
+++ ArcadeTestProject/src/PlayGame/Main.java
@@ -0,0 +1,14 @@
+package PlayGame;
+
+import Entities.ExampleBounce;
+import gameController.GameController;
+
+public class Main 
+{
+
+    public static void main(String[] args)
+    {
+        GameController.main(); // Changed from using "launch()" to "run()"
+    }
+
+}
---
+++ ArcadeTestProject/src/interfaces/GameObject.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+import javafx.scene.Node;
+
+public interface GameObject 
+{
+	public Node getView();
+}
