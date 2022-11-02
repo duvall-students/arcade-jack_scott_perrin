ec7964d5593250dfec9da2cbd2a0ffea12e1b442
scottsumrall <ssumrall@elon.edu>
2022-11-02 00:38:04 -0400

    Added variable with getters and setters to control player health

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -5,6 +5,18 @@ import interfaces.GameObject;
 
 public abstract class Player implements Collidable
 {
+	private int health = 3;
 	//TODO: Convert this into player superclass
 	public abstract void collisionBehavior();
+	
+	public void damagePlayer()
+	{
+		health -= health;
+	}
+	
+	public int getHealth()
+	{
+		return health;
+	}
+	
 }
